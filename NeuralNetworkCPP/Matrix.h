55b3a02ed9e24c7e1a4240e150d0f7972d4b52a9
MENG Xiangyi <xianmeng5@ad.cityu.edu.hk>
2019-04-09 18:47:27 +0800

    Add some matrix operators.

--- NeuralNetworkCPP/Matrix.h
+++ NeuralNetworkCPP/Matrix.h
@@ -2,11 +2,16 @@
 #include <vector>
 #include <stdexcept>
 #include <string>
+#include <iostream>
 using namespace std;
 
 enum DataType
 {
-	UNSIGNED=0, INT, FLOAT, DOUBLE, UNKNOWN
+	UNKNOWN= -1, 
+	UNSIGNED=0, // 00
+	INT, // 01
+	FLOAT, // 10
+	DOUBLE, // 11
 };
 template <class T>
 class Matrix
@@ -63,43 +68,127 @@ public:
 		return (T&)_inner[x * this->_dim_y + y];
 	}
 
-	const T& operator+ (const Matrix<T> mat)
+	const Matrix<double>& operator+ (const Matrix<T> mat)
 	{
 		if (mat.GetDimX() != this->GetDimX() || mat.GetDimY() != this->GetDimY())
 		{
 			throw length_error("Matrices should have same dimensions.");
 		}
-		
+		// Decide the data type
+		if (this->GetDataType() < 0 || mat.GetDataType() < 0)
+		{
+			throw logic_error("Matrices data type unknown");
+		}
+
+		Matrix<double> *result = new Matrix<double>(this->_dim_x, this->_dim_y);
+
 		for (int i = 0; i < this->_dim_x; i++)
 		{
 			for (int j = 0; j < this->_dim_y; j++)
 			{
-
+				(*result)(i, j) = this->_inner[i*_dim_y + j] + mat(i, j);
 			}
 		}
+
+		return *result;
 	}
-	DataType _getDataType()
+	friend const Matrix<double>& operator*(const double a, const Matrix<T>& mat)
 	{
-		
-		string s(typeid(T).name());
-		switch (s)
+		Matrix<double>* result = new Matrix<double>(mat.GetDimX(), mat.GetDimY());
+		unsigned x = mat.GetDimX(), y = mat.GetDimY();
+
+		for (int i = 0; i < x; i++)
 		{
-		case "unsigned int":
-			return UNSIGNED;
-		case "int":
-			return INT;
-		case "float":
-			return FLOAT;
-		case "DOUBLE":
-			return DOUBLE;
-		default:
-			return UNKNOWN;
+			for (int j = 0; j < y; j++)
+			{
+				(*result)(i, j) = a * mat(i, j);
+			}
 		}
+
+		return *result;
+	}
+	const Matrix<double>& operator*(const Matrix<int>& mat)
+	{
+		if (this->_dim_y != mat.GetDimX())
+		{
+			throw length_error("Matrices size should be matched when multipling.");
+		}
+		Matrix<double>* result = new Matrix<double>(this->_dim_x, mat.GetDimY());
+
+		for (int i = 0; i < this->_dim_x; i++)
+		{
+			for (int j = 0; j < mat.GetDimY(); j++)
+			{
+				for (int k = 0; k < this->_dim_y; k++)
+				{
+					(*result)(i, j) += (*this)(i, k) * mat(k, j);
+				}
+			}
+		}
+
+		return *result;
+	}
+	const Matrix<double>& operator*(const Matrix<double>& mat)
+	{
+		if (this->_dim_y != mat.GetDimX())
+		{
+			throw length_error("Matrices size should be matched when multipling.");
+		}
+		Matrix<double>* result = new Matrix<double>(this->_dim_x, mat.GetDimY());
+
+		for (int i = 0; i < this->_dim_x; i++)
+		{
+			for (int j = 0; j < mat.GetDimY(); j++)
+			{
+				for (int k = 0; k < this->_dim_y; k++)
+				{
+					(*result)(i, j) += (*this)(i, k) * mat(k, j);
+				}
+			}
+		}
+
+		return *result;
+	}
+	inline friend ostream& operator <<(ostream& out, const Matrix<T>& mat)
+	{
+		out << "[Matrix, type:" << const_cast<Matrix<T> &>(mat).GetDataTypeName() << ", size: " << mat.GetDimX() << "*" << mat.GetDimY() << "]" << endl;
+		out << "[";
+		unsigned x = mat.GetDimX(), y = mat.GetDimY();
+		for (int i = 0; i < x; i++)
+		{
+			out << "[";
+			for (int j = 0; j < y; j++)
+			{
+				cout << mat(i, j) << ", ";
+			}
+			out << "]," << endl;
+		}
+		out << "]" << endl;
+		return out;
+	}
+	DataType GetDataType() const
+	{
+		/// TODO: Fix the string siwtch expression bug 
+		string s(typeid(T).name());
+		if (s == "unsigned int")
+			return UNSIGNED;
+		if (s == "int")
+			return INT;
+		if (s == "float")
+			return FLOAT;
+		if (s == "double")
+			return DOUBLE;
+		return UNKNOWN;
+	}
+	string GetDataTypeName() const
+	{
+		string typeName[4] = { "unsigned int", "int", "float", "double" };
+
+		return typeName[this->GetDataType()];
 	}
 private:
 	unsigned _dim_x, _dim_y;
 	vector<T> _inner;
-
 	
 };
 
--- NeuralNetworkCPP/main.cpp
+++ NeuralNetworkCPP/main.cpp
@@ -4,15 +4,17 @@
 
 int main()
 {
-	Matrix<unsigned> mat(1, 2);
+	Matrix<int> mat(1, 2);
 	mat(0, 0) = 3;
 	Matrix<int> mat2(1,2);
-	Matrix<float> mat3(1, 2);
+	Matrix<double> mat3(2, 1);
+	mat3(0, 0) = 3;
 	Matrix<double> mat4(1, 2);
+	cout << mat.GetDimX() << mat2.GetDimY() << endl;
 	cout << mat(0, 0) << " " << mat2(0, 0) << endl;
 	mat2(0, 0) = 33;
 	cout << mat(0, 0) << " " << mat2(0, 0) << endl;
 	// cout << typeid(mat(0,0)).name() << " " << typeid(mat2(0, 0)).name() << " " << typeid(mat3(0, 0)).name() << " " << typeid(mat4(0, 0)).name();
-	cout << mat._getDataType();
+	cout << mat << mat2 << mat3 << mat + mat2 << 2.4 * (mat2 + mat) << mat * mat3;
 	return 0;
 }
